d17a00dfe49293c92cbcba4c8f3ae0ab983bb586
Artashes Grigoryan <artashes.grigoryan@cogepart.com>
2022-02-23 16:41:47 +0100

    initial commit

---
+++ apps/poc-goings-on/jest.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  displayName: 'poc-goings-on',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/poc-goings-on',
+};
